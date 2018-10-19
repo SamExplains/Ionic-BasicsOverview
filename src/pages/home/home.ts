7040e7cc8f55e9d758ba77077b61c86341925f61
Sampuccino <Sampuccino@gmail.com>
2018-10-18 20:51:53 -0700

    Added a simple page to see how navigation works and the pages stack.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {UserPage} from "../user/user";
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  showUsers(){
+    this.navCtrl.push(UserPage);
   }
 
 }
