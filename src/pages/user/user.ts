6bf61723cbdcc3f4bc9636f0e8af271afa5fb33c
Sampuccino <Sampuccino@gmail.com>
2018-10-19 17:20:34 -0700

    Made an example on different types of ionic page events.

--- src/pages/user/user.ts
+++ src/pages/user/user.ts
@@ -12,7 +12,33 @@ export class UserPage {
   constructor(private navCtrl: NavController) {}
 
   onLoadUser(name: string){
-    this.navCtrl.push(ProfilePage, {userName: name});
+    this.navCtrl.push(ProfilePage, {userName: name})
+      .catch( (error) => console.log('Access denied' + error) );
+  }
+
+  ionViewCanEnter() :boolean | Promise<void>{
+    console.log('ionViewCanEnter');
+    const rnd = Math.random();
+    return rnd > 0.5;
+  }
+
+
+  ionViewWillEnter(){
+    console.log('ionViewWillEnter')
+  }
+
+  ionViewDidEnter(){
+    console.log('ionViewDidEnter')
+  }
+
+  ionViewCanLeave(): boolean | Promise<void>{
+  console.log('ionViewCanLeave');
+  return new Promise((resolve, reject) => {
+    setTimeout( () => {
+      resolve();
+    }, 1000)
+  });
+
   }
 
 
